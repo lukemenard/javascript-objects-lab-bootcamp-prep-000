b9582875c2f2378c597476dfcbbe7111a3d7f191
Luke Menard <lukemenard@gmail.com>
2019-03-21 20:26:34 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var obj = { prop: 1 }
+  return Object.assign({}, obj, {[key]:value})
+}
+updateObjectWithKeyAndValue(obj, 'prop2', 2)
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  var obj = { prop: 1 }
+  object[key] = value
+  return object
+}
+estructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', 2)
+
+function deleteFromObjectByKey(object, key) {
+  
+}
